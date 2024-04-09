40257787a5d8462d7f2b0c65d1cf50b4ee54879b
milabatista <47610827+milabatista@users.noreply.github.com>
2024-04-09 20:54:27 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,37 +1,58 @@
-import mysql.connector
-from mysql.connector import Error
+# import mysql.connector
+# from mysql.connector import Error
 
-def create_server_connection(host_name, user_name, user_password, db_name):
+# def create_server_connection(host_name, user_name, user_password, db_name):
+#     connection = None
+#     try:
+#         connection = mysql.connector.connect(
+#             host=host_name,
+#             user=user_name,
+#             passwd=user_password,
+#             database=db_name
+#         )
+#         print("MySQL Database connection successful")
+#     except Error as err:
+#         print(f"Error: '{err}'")
+
+#     return connection
+
+# def execute_query(connection, query):
+#     cursor = connection.cursor()
+#     try:
+#         cursor.execute(query)
+#         connection.commit()
+#         print("Query successful")
+#     except Error as err:
+#         print(f"Error: '{err}'")
+
+
+# '''
+# CREATE TABLE items (
+#     id VARCHAR(36) PRIMARY KEY,
+#     name VARCHAR(255) NOT NULL,
+#     description TEXT,
+#     price DECIMAL(10, 2) NOT NULL,
+#     on_offer BOOLEAN DEFAULT FALSE
+# );
+# '''
+
+import pymongo
+from pymango import MongoClient
+
+def create_server_connection(host_name, db_name):
     connection = None
     try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
+        client = MongoClient(host=host_name, port=27017)
+        connection = client[db_name]
         print("MySQL Database connection successful")
     except Error as err:
         print(f"Error: '{err}'")
 
-    return connection
+     return connection
 
-def execute_query(connection, query):
-    cursor = connection.cursor()
+def execute_query(connection, collection, method, object):
     try:
-        cursor.execute(query)
-        connection.commit()
+        connection.collection.method(object)
         print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")
-
-
-'''
-CREATE TABLE items (
-    id VARCHAR(36) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    on_offer BOOLEAN DEFAULT FALSE
-);
-'''
